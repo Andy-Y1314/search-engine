9b80ffdf65c7b1d4a49abf5ac35a65d8fd680dfd
Andy-Y1314 <yanandy1804@gmail.com>
2026-09-07 22:06:05 +0100

    Add file reading

--- search_engine.cpp
+++ search_engine.cpp
@@ -5,8 +5,18 @@ int main(int argc, char* argv[]) {
     if (argc !=5 || strcmp(argv[1], "-d") || strcmp(argv[3], "-k")) {
         cout << "Wrong Arguments!" << endl;
         return -1; 
-    } else {
-        cout << "Correct Arguments" << endl;
+    } 
+
+    int line_counter = 0;
+    int max_length = -1;
+    int k = atoi(argv[4]);
+
+    ifstream file(argv[2]);
+
+    if (!file) {
+        cout << "Error opening file" << endl;
+        return -1;
     }
+    
     return 0;
 }
