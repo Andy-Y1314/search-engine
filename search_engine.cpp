d92294df3fa958d1844e7aae114ce2a0cf844664
Andy-Y1314 <yanandy1804@gmail.com>
2026-09-07 21:39:38 +0100

    Starting error checks for wrong arguments

--- search_engine.cpp
+++ search_engine.cpp
@@ -1,5 +1,12 @@
 #include "search_engine.h"
 
+using namespace std;
 int main(int argc, char* argv[]) {
-
+    if (argc !=5 || strcmp(argv[1], "-d") || strcmp(argv[3], "-k")) {
+        cout << "Wrong Arguments!" << endl;
+        return -1; 
+    } else {
+        cout << "Correct Arguments" << endl;
+    }
+    return 0;
 }
